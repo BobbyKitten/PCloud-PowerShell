fa0254ae606574830a39847842e61d391ad6a2b9
Pavel Kondratev <pavelgeme2@gmail.com>
2017-08-03 18:44:48 +0300

    Change type for *Id params to string, Add download cmdlet

--- PCloud.ps1
+++ PCloud.ps1
@@ -13,11 +13,14 @@ function New-PCloudTocken {
         [Parameter(Mandatory=$true)]
         [string]$Username,
         [Parameter(Mandatory=$true)]
-        [string]$Password
+        [string]$Password,
+        [switch]$SaveTocken
     )
     $unswer = Invoke-WebRequest -Uri "api.pcloud.com/userinfo?getauth=1&logout=1&username=$Username&password=$Password" | ConvertFrom-Json
     if ($unswer.result -eq 0) {
-        echo $unswer.auth > .\PCloud.tocken
+        if ($SaveTocken -eq $true) {
+            echo $unswer.auth > .\PCloud.tocken
+        }
         return $unswer.auth
     }
     return $unswer.error
@@ -27,14 +30,14 @@ function Get-PCloudFiles {
     [CmdletBinding()]
     Param(
         [string]$Path = "/",
-        [int]$FolderId = -1,
+        [string]$FolderId = "-1",
         [switch]$NoFiles
     )
     $tocken = Get-PCloudTocken
     if ($tocken -eq $null) { return $null }
     $url = "https://api.pcloud.com/listfolder?auth=$tocken"
     # Select if we will use path or FolderId
-    if ($FolderId -eq -1) { $url = $url + "&path=$Path" } else { $url = $url + "&folderid=$FolderId" }
+    if ($FolderId -eq "-1") { $url = $url + "&path=$Path" } else { $url = $url + "&folderid=$FolderId" }
     # Remove files, just folders, just hardcore!
     if ($NoFiles -eq $true) { $url = $url + "&nofiles=1" }
     $unswer = Invoke-WebRequest -Uri $url | ConvertFrom-Json
@@ -48,7 +51,7 @@ function Upload-PCloudFile {
     [CmdletBinding()]
     Param (
         [string]$Path = "/",
-        [int]$FolderId = -1,
+        [string]$FolderId = "-1",
         [Parameter(Mandatory=$true)]
         [string]$File,
         [string]$FileName = "",
@@ -58,7 +61,7 @@ function Upload-PCloudFile {
     if ($tocken -eq $null) { return "error!" }
     $uri = "https://api.pcloud.com/uploadfile?auth=$tocken&filename=$FileName"
     # Select if we will use path or FolderId
-    if ($FolderId -eq -1) { $uri += "&path=$Path" } else { $uri += "&folderid=$FolderId" }
+    if ($FolderId -eq "-1") { $uri += "&path=$Path" } else { $uri += "&folderid=$FolderId" }
     # If file exists, then rename it
     if ($RenameIfExists -eq $true) { $uri = $uri + "&renameifexists=1" }
 
@@ -69,7 +72,31 @@ function Upload-PCloudFile {
 }
 
 function Download-PCloudFile {
-    Write-Warning "Work in progress"
+    [CmdletBinding()]
+    Param (
+        [string]$FileName = "",
+        [string]$FileId = "-1",
+        [Parameter(Mandatory=$true)]
+        [string]$FileOut
+    )
+    $tocken = Get-PCloudTocken
+    if ($tocken -eq $null) { return "error!" }
+
+    $url_getlink = "https://api.pcloud.com/getfilelink?auth=$tocken"
+    
+    if ($FileId -eq "-1") { $url_getlink += "&path=$FileName" } else { $url_getlink += "&fileid=$FileId" }
+    
+    $links = Invoke-WebRequest -Uri $url_getlink | ConvertFrom-Json
+
+    if ($links.result -eq 0) {
+        $wc = New-Object System.Net.WebClient
+        $server_name = $links.hosts[0]
+        $server_path = $links.path
+        [byte[]]$result = $wc.DownloadFile("http://$server_name$server_path", $FileOut)
+        return "Success"
+    }
+
+    return $links.error
 }
 
 function New-PCloudFolder {
